29095fbb89c27a69ed311bcf9a0585eb3c5179e6
Daniel Schneider <daschne8@gmail.com>
2019-04-10 15:34:03 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,8 +1,22 @@
 class Pokemon
   attr_accessor :id,:name,:type,:db
   @@all = []
+
   def initialize(attributes)
     attributes.each{|key,value| self.send("#{key}=",value)}
+    self.all << self
+  end
+
+  def all
+    @@all
+  end
+
+  def self.save
+    
+  end
+
+  def self.find()
+    
   end
 
 end
